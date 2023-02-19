80dc96e6b99e59ca9519cffa791e34de2af6f90a
JOHANN NGEW <212061N@mymail.nyp.edu.sg>
2023-02-19 23:15:00 +0800

    Finally got sparks to spawn on surface

--- Assets/Weapons/ProjectileBase.cs
+++ Assets/Weapons/ProjectileBase.cs
@@ -14,6 +14,8 @@ public class ProjectileBase : MonoBehaviour
     public GameObject bloodEffect;
     public GameObject sparkEffect;
 
+    // Used to debug particle effect spawn location using ray tracing. Comment out destroy and debug.drawray to use
+    protected Vector3 debugOnTriggerBackwardsPosition;
     public void SetCreator(GameObject _creator)
     {
         creator = _creator;
@@ -30,11 +32,18 @@ public class ProjectileBase : MonoBehaviour
 
     public void Update()
     {
+        //Debug.DrawRay(this.transform.position, (debugOnTriggerBackwardsPosition - this.transform.position), Color.green);
+
         elapsed += Time.deltaTime;
         if(elapsed >= duration)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void FixedUpdate()
+    {
+     
     }
 
     // Checks if a collision with a hitbox that belongs to a player, and returns true if player is same as the one who created that projectile
@@ -55,15 +64,21 @@ public class ProjectileBase : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //Vector3 vel = this.GetComponent<Rigidbody>().velocity;
-        //Ray laserRayCast = new Ray(this.transform.position + (-vel.normalized * 0.1f), vel);
-        //if (Physics.Raycast(laserRayCast, out RaycastHit hit, 50))
-        //{
-        //    GameObject effect = Instantiate(sparkEffect, particleManager.transform);
-        //    effect.transform.position = this.transform.position;
-        //    effect.transform.rotation = Quaternion.Euler(hit.normal);
-        //}
+        Vector3 vel = this.GetComponent<Rigidbody>().velocity;
+        debugOnTriggerBackwardsPosition = this.transform.position - vel.normalized * 1;
+        Ray laserRayCast = new Ray(debugOnTriggerBackwardsPosition, vel);
+        if (Physics.Raycast(laserRayCast, out RaycastHit hit, 1))
+        {
+            GameObject effect = Instantiate(sparkEffect, particleManager.transform);
+            effect.transform.position = hit.point;
+            //Debug.LogError(DebugSavePosition);
+            effect.transform.rotation = Quaternion.LookRotation(hit.normal);
+        }
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        this.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+
         Destroy(gameObject);
+
     }
 
 }
