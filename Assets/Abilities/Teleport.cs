1c6f194ae058c1b1005d2280de2390fc48cd1f44
jiulen <wongjiulen.coding@gmail.com>
2023-02-20 15:02:16 +0800

    Improved teleport and moved to separate script

---
+++ Assets/Abilities/Teleport.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    enum TeleportStates
+    {
+        NONE,
+        TELEPORT_MARKER,
+        TELEPORT_CHANNEL,
+    };
+    TeleportStates teleportState = TeleportStates.NONE;
+    const float teleportDuration = 1.0f, teleportCooldown = 1.0f;
+    float teleportProgress = 0.0f, teleportCooldownTimer = teleportCooldown;
+    float teleportDistance = 10.0f;
+    [SerializeField] GameObject tpMarkerPrefab;
+    GameObject tpMarker;
+    LayerMask terrain;
+
+    Transform playerHead;
+    FPS playerFPSScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tpMarker = Instantiate(tpMarkerPrefab);
+        playerFPSScript = GetComponent<FPS>();
+        playerHead = playerFPSScript.head.transform;
+
+        terrain = 1 << LayerMask.NameToLayer("Terrain");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerFPSScript.canTeleport)
+        {
+
+            UpdateTeleport();
+        }
+    }
+
+    private void UpdateTeleport()
+    {
+        switch (teleportState)
+        {
+            case TeleportStates.NONE:
+                {
+                    if (Input.GetKeyDown(KeyCode.Alpha1) && teleportCooldownTimer >= teleportCooldown)
+                        teleportState = TeleportStates.TELEPORT_MARKER;
+
+                    teleportCooldownTimer += Time.deltaTime;
+
+                    break;
+                }
+            case TeleportStates.TELEPORT_MARKER:
+                {
+                    if (Physics.Raycast(playerHead.position, playerHead.forward, out RaycastHit raycastForwardHit, teleportDistance, terrain))
+                    {
+                        if (Physics.Raycast(raycastForwardHit.point + new Vector3(0, 1.5f, 0), Vector3.down, out RaycastHit raycastDownHit, 3f, terrain))
+                        {
+                            tpMarker.transform.position = raycastDownHit.point + new Vector3(0, tpMarker.transform.localScale.y * 0.5f, 0);
+                            tpMarker.SetActive(true);
+                            if (Input.GetKeyDown(KeyCode.Alpha1))
+                            {
+                                teleportProgress = 0.0f;
+                                teleportState = TeleportStates.TELEPORT_CHANNEL;
+                            }
+                        }
+                        else
+                            tpMarker.SetActive(false);
+                    }
+                    else
+                        tpMarker.SetActive(false);
+
+                    break;
+                }
+            case TeleportStates.TELEPORT_CHANNEL:
+                {
+                    if (teleportProgress < teleportDuration)
+                    {
+                        //Channel teleport
+                        teleportProgress += Time.deltaTime;
+                    }
+                    else
+                    {
+                        //Do teleport
+                        transform.position = tpMarker.transform.position + new Vector3(0, 1f, 0);
+
+                        teleportState = TeleportStates.NONE;
+                        teleportCooldownTimer = 0;
+                        tpMarker.SetActive(false);
+                    }
+
+                    break;
+                }
+        }
+    }
+}
