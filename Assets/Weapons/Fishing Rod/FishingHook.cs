89475419ea91ad499628bf7ec01f07770510bbef
jiulen <wongjiulen.coding@gmail.com>
2023-02-22 15:52:07 +0800

    Fishing Rod done

--- Assets/Weapons/Fishing Rod/FishingHook.cs
+++ Assets/Weapons/Fishing Rod/FishingHook.cs
@@ -6,7 +6,14 @@ public class FishingHook : MonoBehaviour
 {
     [SerializeField] FishingRod fishingRod;
 
+    ParticleManager particleManager;
+
     float gravity = -9.81f;
+
+    private void Start()
+    {
+        particleManager = GameObject.Find("Particle Manager").GetComponent<ParticleManager>();
+    }
 
     private void FixedUpdate()
     {
@@ -24,8 +31,24 @@ public class FishingHook : MonoBehaviour
     {
         if (fishingRod.hookState == FishingRod.HookState.NOT_HOOKED)
         {
-            Debug.Log("Hooked");
+            transform.position = other.ClosestPointOnBounds(transform.position);
+
             //Check what is hooked
+            if (other.tag == "PlayerHitBox")
+            {
+                if (other.name == "Head")
+                {
+                    particleManager.CreateEffect("Blood_PE", transform.position, -fishingRod.hookVelocity.normalized, 15);
+                }
+                else
+                {
+                    particleManager.CreateEffect("Blood_PE", transform.position, -fishingRod.hookVelocity.normalized);
+
+                }
+                fishingRod.hookedRigidbody = other.attachedRigidbody;
+            }
+            else
+                particleManager.CreateEffect("Sparks_PE", transform.position, -fishingRod.hookVelocity.normalized);
 
             fishingRod.hookState = FishingRod.HookState.HOOKED;
         }
